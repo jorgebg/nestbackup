9118e85ab44f1d6f8f8b8eb7b45e09449e620a79
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2021-06-19 22:06:32 +0200

    version 0.0.3

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/jorgebg/nestbackup'
 EMAIL = 'jorge.barata.gonzalez@gmail.com'
 AUTHOR = 'Jorge Barata'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.2'
+VERSION = '0.0.3'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
